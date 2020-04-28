226bfda6adb79f19b8fe974ecbbb9497232520b6
EaglesDock <50552688+EaglesDock@users.noreply.github.com>
2020-04-28 19:02:28 +1200

    Update Chapter13-OOP-encapsulation.py

--- Chapter13-OOP-encapsulation.py
+++ Chapter13-OOP-encapsulation.py
@@ -25,3 +25,27 @@ class Data:
 #contains instance variables. which is nums. Nums contains a list of integers
 #Object is data, created as a class, class = object.
 #to change the items change_data METHOD. OR direct access to nums instance variable
+
+
+>>> class Data:
+    def __init__(self):
+        self.nums = [1, 2, 3, 4, 5]
+
+    def change_data(self, index, n):
+        self.nums[index] = n
+
+>>> data_one = Data()
+>>> data_one.nums[0] = 100
+>>> print(data_one.nums)
+[100, 2, 3, 4, 5]
+
+>>> data_two = Data()
+>>> data_two.change_data(0, 100)
+>>> print(data_two.nums)
+[100, 2, 3, 4, 5]
+
+##Class Data, Data is the object.
+##Def is defining it, __init__ is initializing it.
+##the variables is held in a list.
+##Change_data is a method.
+
